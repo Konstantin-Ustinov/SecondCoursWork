e69f0c2a3c6241a3d100312fbbebf84ca655f1bf
Konstantin-Ustinov <ustinov.kn@mail.ru>
2023-01-05 23:31:48 +0300

    first commit

---
+++ src/main/java/com/secondcourswork/SecondCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package com.secondcourswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondCourseWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondCourseWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/secondcourswork/SecondCourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondcourswork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondCourseWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
